498dc6b0f7e3ee092e22eac2b2bed37a06f4d6c5
TJ Holowaychuk <tj@vision-media.ca>
2012-06-27 13:46:31 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+
+/**
+ * Module dependencies.
+ */
+
+var crypto = require('crypto');
+
+/**
+ * Bytesize.
+ */
+
+var len = 128;
+
+/**
+ * Iterations. ~300ms
+ */
+
+var iterations = 12000;
+
+/**
+ * Set length to `n`.
+ *
+ * @param {Number} n
+ * @api public
+ */
+
+exports.length = function(n){
+  len = n;
+};
+
+/**
+ * Set iterations to `n`.
+ *
+ * @param {Number} n
+ * @api public
+ */
+
+exports.iterations = function(n){
+  iterations = n;
+};
+
+/**
+ * Hashes a password with optional `salt`, otherwise
+ * generate a salt for `pass` and invoke `fn(err, salt, hash)`.
+ *
+ * @param {String} password to hash
+ * @param {String} optional salt
+ * @param {Function} callback
+ * @api public
+ */
+
+exports.hash = function (pwd, salt, fn) {
+  if (3 == arguments.length) {
+    crypto.pbkdf2(pwd, salt, iterations, len, fn);
+  } else {
+    fn = salt;
+    crypto.randomBytes(len, function(err, salt){
+      if (err) return fn(err);
+      salt = salt.toString('base64');
+      crypto.pbkdf2(pwd, salt, iterations, len, function(err, hash){
+        if (err) return fn(err);
+        fn(null, salt, hash);
+      });
+    });
+  }
+};
